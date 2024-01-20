33feb94f957b4c52050d78c55b1e09a6252e2173
Salwa-mansour <alqadasisal@gmail.com>
2024-01-20 21:35:52 +0300

    calculator working still there are bugs when getting minus value

--- app.js
+++ app.js
@@ -1,61 +1,81 @@
-let form = document.querySelector('.calculator form')
-let display = document.querySelector('#display input')
+let form = document.querySelector('.calculator form');
+let opratorBtns = document.querySelectorAll('.calculator .oprator');
+let display = document.querySelector('#display input');
 let storge =0;
+
 form.addEventListener("click",changeDisplayText)
+if(display.value===""){
+    disableBtns();
+   }
 
 function changeDisplayText(e){
     switch (e.target.className) {
         case 'AC':
             display.value = "";
             storge = 0;
+            disableBtns();
         break;
         case 'button':
+            
+            display.value += e.target.value;
+            opratorBtns.forEach(btn=>{
+                btn.classList.remove('disable');
+            })
+        break; 
         case 'oprator':  
             display.value += e.target.value;
-        break;    
+            disableBtns();
+    break;    
         case 'DE':
            display.value= display.value.toString().slice(0,-1);
+           if(display.value===""){
+            disableBtns();
+           }
         break;
-        case 'oprator':
-            performOprations(e);
-            break;
         case 'equal':
-            console.log('equal')
-            break
+          performOprations();
+        break;
     }
-    // if(e.target.className == 'button'){
-    //     display.value += e.target.value;
-
-    // }else if(e.target.id == 'AC'){
-    //     display.value = "";
-    // }else if(e.target.id=='DE'){
-        
-    //     display.value= display.value.toString().slice(0,-1);
    
-    // }
 }
 
 function performOprations(e){
- let values = display.value.split(/\W/)
-switch (e.target.value) {
-    case '+':
-       
-        storge = values.reduce((total, currentValue) => {
-            return total + Number(currentValue);
-        }, 0)
-        console.log("fgd")
-    break;
-    case '-':
-    
-    break;    
-    case '/':
-      
-    break;
-    case '*':
-       
-    break;
-   
+ let values = display.value.split(/[+\-/*]/g);
+ let opartors =display.value.split(/\d+/g);
+ opartors.shift();opartors.pop();//remove empty strings
+
+ if(values[0]!=null ){
+
+  storge=Number(values[0]) //set inial value
+        for(i=0;i<opartors.length;i++){
+            switch (opartors[i]) {
+                case '+':
+                console.log(`${storge}+=${values[i+1]}`) 
+                storge+=Number(values[i+1]); 
+                break;
+                case '-':
+                console.log(`${storge}-=${values[i+1]}`)
+                storge-=Number(values[i+1]);
+                break;    
+                case '/':
+                console.log(`${storge}/=${values[i+1]}`)
+                storge/=Number(values[i+1]);
+                break;
+                case '*':
+                console.log(`${storge}*=${values[i+1]}`)
+                storge*=Number(values[i+1]);
+                break;
+            }
+        }
+ 
+ }
+
+display.value=storge;
 }
 
-
+function disableBtns(){
+    opratorBtns.forEach(btn=>{
+       btn.classList.add('disable');
+   })
+   console.log('boo')
 }
